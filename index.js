de0d32b4f308e6b72d7ab70d1b5ee78a482472ad
ifeoma101 <fajife11@googlemail.com>
2024-03-22 10:45:38 +0000

    Added time colour changes, (past, present, future)

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 function saveTimeblock(event) {
-event.preventDefault()
-console.log(event.target) 
-console.log(event.target.previousElementSibling)
-let plannerObj = {
-time: event.target.getAttribute("id"),
-text:event.target.previousElementSibling.innerText
+  event.preventDefault();
+  console.log(event.target);
+  console.log(event.target.previousElementSibling.value);
+  let plannerObj = {
+    time: event.target.getAttribute("id"),
+    text: event.target.previousElementSibling.value,
+  };
 
+  // Google JSON.stringify and JSON.parse()
+  // Creatng a key name and giving it a value and storing it in local storage
+  localStorage.setItem(plannerObj.time, plannerObj.text);
+  // accessing value by using the key name
+  //const name = localStorage.getItem(plannerObj.time)
+  console.log(plannerObj);
 }
-console.log(plannerObj);
-}
-
+$(document).ready(function () {
+  $(".text-area").each(function () {
+    let dataValue = $(this).data("hour");
+    console.log(dataValue);
+    let localValue = localStorage.getItem(dataValue);
+    $(this).val(localValue);
+    let currentHour = dayjs().hour();
+console.log(currentHour);
+    $("#currentDay").text(dayjs().format("dd-MM-YYYY"));
+    if (dataValue < currentHour) {
+      $(this).addClass("past");
+    } else if (dataValue > currentHour) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+});
